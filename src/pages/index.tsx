1825ef4aafd61cf718ecf30134ee55ffe54388fd
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-01 14:16:39 -0400

    removed import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { trpc } from '@/utils/trpc'
-import NavBar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 

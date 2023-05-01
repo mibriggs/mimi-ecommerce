879096812c4d5bc11860015557425e9b6be2da4c
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-04-30 21:34:16 -0400

    greeting to mimi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const greetingQuery = trpc.getGreeting.useQuery();
   
-  if (greetingQuery.isSuccess) {
-    console.log(greetingQuery.data);
+  if (greetingQuery.isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (greetingQuery.isError) {
+    return <span>An Error Occurred</span>
   }
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      {greetingQuery.data}
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/pages/index.tsx</code>
@@ -119,7 +124,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }

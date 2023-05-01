47e2feb4e785f6c56a1e204498734425938a947a
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-01 13:46:05 -0400

    Initial commit for stripe payments

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+export const NavBar = () => {
+    return (
+        <div className="flex-col">
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+            <div>Item 5</div>
+        </div>
+    );
+};
+
+export default NavBar;

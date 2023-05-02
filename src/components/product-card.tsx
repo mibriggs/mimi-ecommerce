3d2d5880e7120f30ebc76fb87c9adafa20c2feda
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-01 21:25:48 -0400

    Plus minus functionality

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 const ProductCard = () => {
+    const [quantity, setQuantity] = useState(0);
+    const updateQuantity = (isIncrease: boolean) => {
+        if (isIncrease) {
+            if (quantity < 100) {
+                setQuantity(quantity+1);
+            }
+        }
+        else {
+            if (quantity > 0) {
+                setQuantity(quantity-1);
+            }
+        }
+        // isIncrease? setQuantity(quantity+1) : setQuantity(quantity-1);
+    }
     return (
         <div className="bg-white text-gray-900 w-fit flex flex-col flex-wrap items-center m-8 p-5 rounded-2xl">
             <h1>Product Name</h1>
@@ -6,9 +22,9 @@ const ProductCard = () => {
             <div>Product Description</div>
             <div>Price?</div>
             <div className="flex flex-row">
-                <button className="w-8 bg-red-600 rounded-md"> - </button>
-                <text>Quantity Button</text>
-                <button className="w-8 bg-green-600 rounded-md"> + </button>
+                <button className="w-8 bg-red-600 rounded-md" onClick={() => updateQuantity(false)}> - </button>
+                <div>{quantity}</div>
+                <button className="w-8 bg-green-600 rounded-md" onClick={() => updateQuantity(true)}> + </button>
             </div>
             <div>Add to cart</div>
         </div>

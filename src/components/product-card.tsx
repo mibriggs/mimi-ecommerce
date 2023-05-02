3613f81587b5878e5328b7d1384dc3731caa5340
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-01 20:51:29 -0400

    Added more criteria to product card

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -2,12 +2,15 @@ const ProductCard = () => {
     return (
         <div className="bg-white text-gray-900 w-fit flex flex-col flex-wrap items-center m-8 p-5 rounded-2xl">
             <h1>Product Name</h1>
+            <div>Product Image?</div>
             <div>Product Description</div>
+            <div>Price?</div>
             <div className="flex flex-row">
-                <button className="w-8 bg-red-600/75 rounded-md"> - </button>
-                <div>Quantity Button</div>
+                <button className="w-8 bg-red-600 rounded-md"> - </button>
+                <text>Quantity Button</text>
                 <button className="w-8 bg-green-600 rounded-md"> + </button>
             </div>
+            <div>Add to cart</div>
         </div>
     );
 };

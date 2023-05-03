c648207d290d97da56f9744726517aadb4c65c25
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-03 13:53:03 -0400

    Added mimi's custom font

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,15 @@
 import NavBar from "./nav-bar";
+import localFont from 'next/font/local';
 import MimiIcon from "../media/mimi-icon.png";
 import Image from "next/image";
+
+const customFont = localFont({src: '../media/Mimi-Regular.ttf'});
 
 const Layout = ({ children }: { children: JSX.Element }) => {
     return (
         <div className="flex flex-col h-screen">
             {/* <Image src={MimiIcon} alt={"me"}/> */}
-            <div className="bg-mimilicious text-center text-eggshell p-8">Mimi Gurumi</div>
+            <div className={`bg-mimilicious text-center text-eggshell p-8 ${customFont.className} text-7xl`}>MimiGurumi's</div>
             <NavBar />
             <div className="bg-oceanview h-screen">
                 {children}

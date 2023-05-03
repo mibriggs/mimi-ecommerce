8976094bf8ea67cf23f104d91cf9da7598f57194
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-03 14:07:33 -0400

    switched out apostrophe

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import NavBar from "./nav-bar";
 import localFont from "next/font/local";
-// import MimiIcon from "../media/mimi-icon.png";
-// import Image from "next/image";
+import MimiIcon from "../media/mimi-icon.png";
+import Image from "next/image";
 
 const customFont = localFont({src: '../media/Mimi-Regular.ttf'});
 
@@ -9,7 +9,7 @@ const Layout = ({ children }: { children: JSX.Element }) => {
     return (
         <div className="flex flex-col h-screen">
             {/* <Image src={MimiIcon} alt={"me"}/> */}
-            <div className={`bg-mimilicious text-center text-eggshell p-8 ${customFont.className} text-8xl`}>MimiGurumi's</div>
+            <div className={`bg-mimilicious text-center text-eggshell p-8 ${customFont.className} text-8xl`}>MimiGurumi&apos;s</div>
             <NavBar />
             <div className="bg-oceanview h-screen">
                 {children}

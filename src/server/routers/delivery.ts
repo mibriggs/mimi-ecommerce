b5f9906549a8395bbfb0437011bd8a2c4138828b
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-11 18:48:46 -0400

    create routers for product and deliveries

---
+++ src/server/routers/delivery.ts
@@ -0,0 +1,6 @@
+import { router } from "../trpc";
+
+export const deliveryRouter = router({
+    // createDelivery/updateDelivery/getDelivery/deleteDelivery should all be protectedRoutes
+    // probably want a convenience endpoint to getAllDeliveries
+});
--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -2,18 +2,14 @@ import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
 export const userRouter = router({
-    createUser: publicProcedure
+    createUser: publicProcedure // Creating a user should probably be public
     .input(z.object({
         name: z.string().min(1)
     }))
     .mutation(async ({input, ctx}) => {
         const name = input.name;
-        // const newUser = await ctx.prisma.user.create({
-        //     data: {
-        //         firstName: name,
-                
-        //     }
-        // });
-        // return newUser;
     }),
+
+    // updateUser/deleteUser/getUser should all be protectedRoutes
+    // probably want a convenience endpoint to getAllUsers
 });

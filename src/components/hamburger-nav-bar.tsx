6752c5be5e8010b8079b7a77db73ca5ee9c87db9
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-13 00:54:54 -0400

    mapped some of the buttons

--- src/components/hamburger-nav-bar.tsx
+++ src/components/hamburger-nav-bar.tsx
@@ -3,16 +3,25 @@ import { useRouter } from 'next/router';
 import { Key, useState } from 'react';
 import MimiIcon from '../media/mimi-icon.png';
 import HamburgerIcon from '../media/hamburger-icon.svg';
-import { Button, Dialog, DialogTrigger, Heading, Modal, ModalOverlay } from 'react-aria-components';
+import XIcon from '../media/x-icon.svg';
+import { Button, Dialog, DialogTrigger, Modal, ModalOverlay } from 'react-aria-components';
 import localFont from 'next/font/local';
+import { Itim } from 'next/font/google'
 
 const customFont = localFont({ src: '../media/Mimi-Regular.ttf' });
+const itim = Itim({
+    weight: '400',
+    subsets: ['latin']
+});
+
 
 const HamburgerNavBar = () => {
     const router = useRouter();
     const [isOpen, setIsOpen] = useState(false);
-    const handleClick = (key: Key) => {
-        router.push(key.toString());
+    
+    const handleClick = (key: string, close: () => void) => {
+        router.push(key);
+        close();
     }
     //! This is incomplete!!!!!!
     return (
@@ -23,10 +32,24 @@ const HamburgerNavBar = () => {
                 <Button onPress={() => setIsOpen(true)} >
                     <Image src={HamburgerIcon} alt='Hamburger Menu Button' quality={100} className='pr-3' />
                 </Button>
-                <ModalOverlay className='fixed inset-0 bg-black bg-opacity-50'>
-                    <Modal isDismissable isOpen={isOpen} onOpenChange={() => setIsOpen(false)} className='fixed top-5 bottom-0 right-0 bg-eggshell rounded-tl-3xl rounded-bl-3xl w-7/12'>
+                <ModalOverlay className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md'>
+                    <Modal isDismissable isOpen={isOpen} onOpenChange={() => setIsOpen(false)} className='fixed top-5 bottom-0 right-0 bg-eggshell rounded-tl-3xl rounded-bl-3xl w-7/12 p-8 text-black'>
                         <Dialog>
-                            Modal
+                            {({close}) => 
+                            <div className={`${itim.className} flex flex-col text-xl`}>
+                                <Button onPress={close} className='self-end'>
+                                    <Image src={XIcon} alt='Close Button' quality={100} />
+                                </Button>
+                                <Button onPress={() =>  handleClick('/', close)} className='text-left'>Home</Button>
+                                <Button onPress={() => handleClick('/products', close)} className='text-left'>Products</Button>
+                                <Button onPress={() => handleClick('/about', close)} className='text-left'>About Us</Button>
+                                <Button onPress={() => handleClick('/contact-us', close)} className='text-left'>Contact Us</Button>
+                                <Button className='text-left'>Donate</Button>
+                                <div className='flex flex-col space-y-5'>
+                                    <Button className='bg-neutral-300 rounded-lg h-11'>Sign In</Button>
+                                    <Button className='bg-oceanview rounded-lg h-11'>Sign Up</Button>
+                                </div>
+                            </div>}
                         </Dialog>
                     </Modal>
                 </ModalOverlay>

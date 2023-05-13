cfcb764001521608a4a1f1f82fe2361ad4a4818b
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-12 23:45:33 -0400

    working on modal

--- src/components/hamburger-nav-bar.tsx
+++ src/components/hamburger-nav-bar.tsx
@@ -1,38 +1,37 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Key } from 'react';
-import {MenuTrigger, Button, Popover, Menu, Item} from 'react-aria-components';
-import HamburgerMenuIcon from '../media/burger-bar.png';
+import { Key, useState } from 'react';
+import MimiIcon from '../media/mimi-icon.png';
+import HamburgerIcon from '../media/hamburger-icon.svg';
+import { Button, Dialog, DialogTrigger, Heading, Modal, ModalOverlay } from 'react-aria-components';
+import localFont from 'next/font/local';
+
+const customFont = localFont({ src: '../media/Mimi-Regular.ttf' });
 
 const HamburgerNavBar = () => {
     const router = useRouter();
+    const [isOpen, setIsOpen] = useState(false);
     const handleClick = (key: Key) => {
         router.push(key.toString());
     }
     //! This is incomplete!!!!!!
     return (
-    <MenuTrigger>
-        {/* <Button aria-label="Menu" className="bg-mimilicious w-auto text-bold">☰</Button> */}
-        <Button aria-label="Menu" className="bg-mimilicious w-auto text-bold">
-            <Image 
-            src={HamburgerMenuIcon}
-            alt='Hamburger Menu Icon'
-            height={20}
-            width={500}
-            />
-        </Button>
-        {/* <a href="https://www.flaticon.com/free-icons/menu" title="menu icons">Menu icons created by Febrian Hidayat - Flaticon</a> */}
-        <Popover>
-            <Menu className='bg-eggshell text-black' onAction={(key: Key) => handleClick(key)}>
-                <Item id='/'>Home</Item> 
-                <Item id="/products">Products</Item>
-                <Item id="/about">About Us</Item>
-                <Item id="/contact-us">Contact Us</Item>
-                <Item id="donte">Donate</Item>
-                <Item id="checkout">Checkout</Item>
-            </Menu>
-        </Popover>
-    </MenuTrigger>
+        <div className={`md:hidden flex flex-row bg-mimilicious ${customFont.className} text-3xl items-center justify-between`}>
+            <Image src={MimiIcon} alt={'Gurumi Icon'} width={79} height={75} quality={100} className='rotate-15' />
+            MimiGurumi&apos;s            
+            <DialogTrigger>
+                <Button onPress={() => setIsOpen(true)} >
+                    <Image src={HamburgerIcon} alt='Hamburger Menu Button' quality={100} className='pr-3' />
+                </Button>
+                <ModalOverlay className='fixed inset-0 bg-black bg-opacity-50'>
+                    <Modal isDismissable isOpen={isOpen} onOpenChange={() => setIsOpen(false)} className='fixed top-5 bottom-0 right-0 bg-eggshell rounded-tl-3xl rounded-bl-3xl w-7/12'>
+                        <Dialog>
+                            Modal
+                        </Dialog>
+                    </Modal>
+                </ModalOverlay>
+            </DialogTrigger>
+        </div>
     );
 };
 
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,26 +1,9 @@
 import NavBar from '../components/nav-bar';
 import HeaderComponent from '../components/header';
-import MimiIcon from '../media/mimi-icon.png';
-import Image from 'next/image';
 import useViewport from '@/hooks/user-viewport';
+import TestMenu from '@/components/test-hamburger';
 import HamburgerNavBar from '@/components/hamburger-nav-bar';
 
-const HeaderAndHamburger = () => {
-	return (
-	<div className='flex flex-row'>
-		<HamburgerNavBar />
-		<HeaderComponent />
-	</div>);
-};
-
-const HeaderAndMenu = () => {
-	return (
-		<>
-		<HeaderComponent />
-		<NavBar />
-		</>
-	)
-}
 const Layout = ({ children }: { children: JSX.Element }) => {
 	const BREAKPOINT = 620;
 	const { width, height } = useViewport();
@@ -29,8 +12,11 @@ const Layout = ({ children }: { children: JSX.Element }) => {
 	return (
 		<div className='flex h-screen flex-col'>
 			{/* <Image src={MimiIcon} alt={"me"}/> */}
-			{(width >= BREAKPOINT) ? <HeaderAndMenu /> : <HeaderAndHamburger />}
-			<div className='h-screen bg-oceanview'>{children}</div>
+			<HeaderComponent />
+			<NavBar />
+			<HamburgerNavBar />
+			{/* {(width >= BREAKPOINT) ? <HeaderAndMenu /> : <HeaderAndHamburger />} */}
+			<div className='h-screen bg-eggshell md:bg-oceanview'>{children}</div>
 		</div>
 	);
 };

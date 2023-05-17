459801643d7876c3c762298b5a3dd7d6abc8ddf2
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-17 15:28:50 -0500

    Added google and facebook providers?

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,6 +3,8 @@ import { DefaultSession, NextAuthOptions, getServerSession } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from './db/client';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
 
 //! Look into how all this works lol
 declare module 'next-auth' {
@@ -31,10 +33,25 @@ export const authOptions: NextAuthOptions = {
 			clientId: process.env.GITHUB_CLIENT_ID || '',
 			clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
 		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+		}),
+		/**
+		 * I don't think facebook is configured correctly
+		 * Got an error the first time i tried logging in? But it's gone now idk
+		 * Also... when i login with facebook it redirects me here: http://localhost:3000/#_=_ instead of http://localhost:3000?
+		 * @see https://next-auth.js.org/providers/facebook
+		 * @see https://developers.facebook.com/apps/594282442483155/dashboard/
+		 */
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_CLIENT_ID || '',
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
+		}),
 	],
-    // pages: {
-    //     signIn: '/auth/signin',
-    // }
+	// pages: {
+	//     signIn: '/auth/signin',
+	// }
 };
 
 export const getServerAuthSession = (ctx: {

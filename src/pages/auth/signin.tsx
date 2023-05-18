a4ca1797fd739655ae93c49cb9a712eb78a67c69
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-05-17 20:08:47 -0500

    styling login page a bit

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -5,16 +5,44 @@ import type {
 import { getProviders, signIn } from 'next-auth/react';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../server/auth';
+import Image from 'next/image';
+import Facebook from '../../media/facebook.png';
+import Google from '../../media/google.png';
+import GitHub from '../../media/github.png';
 
 export default function SignIn({
 	providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	function getImageName(name: string) {
+		switch (name) {
+			case 'GitHub':
+				return GitHub;
+			case 'Google':
+				return Google;
+			case 'Facebook':
+				return Facebook;
+			default:
+				return '';
+		}
+	}
+
 	return (
 		<>
 			{Object.values(providers).map((provider) => (
 				<div key={provider.name}>
-					<button onClick={() => signIn(provider.id)}>
-						Sign in with {provider.name}
+					<button
+						onClick={() => signIn(provider.id)}
+						className='m-2 rounded-full bg-white px-5 py-1 text-black shadow-md'
+					>
+						<div className='flex flex-row items-center'>
+							<Image
+								src={getImageName(provider.name)}
+								alt={'Provider logo'}
+								width={45}
+								height={45}
+							/>{' '}
+							Sign in with {provider.name}
+						</div>
 					</button>
 				</div>
 			))}
